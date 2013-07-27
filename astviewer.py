e368ec0df48c7e3d459b415db712d09976a14ce2
Pepijn Kenter <titusjan@gmail.com>
2013-07-27 12:25:13 +0200

    Added text editor. Can open files.

--- astviewer.py
+++ astviewer.py
@@ -28,7 +28,7 @@ ABOUT_MESSAGE = u"""%(prog)s version %(version)s
 class AstViewer(QtGui.QMainWindow):
     """ The main application.
     """
-    def __init__(self):
+    def __init__(self, file_name = None):
         """ Constructor
         
             :param ast_tree: list of Figure objects
@@ -41,13 +41,16 @@ class AstViewer(QtGui.QMainWindow):
         self._setup_menu()
         self._setup_views()
         self.setWindowTitle(PROGRAM_NAME)
+        self.open_file(file_name = file_name)
 
               
     def _setup_menu(self):
         """ Sets up the main menu.
         """
         file_menu = QtGui.QMenu("&File", self)
-
+        file_menu.addAction("&New...", self.new_file, "Ctrl+N")
+        file_menu.addAction("&Open...", self.open_file, "Ctrl+O")
+        
         close_action = file_menu.addAction("C&lose", self.close_window)
         close_action.setShortcut("Ctrl+W")
 
@@ -66,15 +69,32 @@ class AstViewer(QtGui.QMainWindow):
     def _setup_views(self):
         """ Creates the UI widgets. 
         """
-        central_widget = QtGui.QWidget()
-        self.setCentralWidget(central_widget)
-        
+        #central_splitter = QtGui.QWidget()
+        central_splitter = QtGui.QSplitter(self, orientation = QtCore.Qt.Horizontal)
+        self.setCentralWidget(central_splitter)
         central_layout = QtGui.QHBoxLayout()
-        central_widget.setLayout(central_layout)
+        central_splitter.setLayout(central_layout)
         
+        # Tree widget
         self.ast_tree = QtGui.QListWidget()
         central_layout.addWidget(self.ast_tree)
         self._fill_ast_tree()
+
+        # Editor widget
+        
+        font = QtGui.QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(12)
+
+        self.editor = QtGui.QTextEdit()
+        self.editor.setFont(font)
+        central_layout.addWidget(self.editor)
+
+        central_splitter.setCollapsible(0, False)
+        central_splitter.setCollapsible(1, False)
+        central_splitter.setStretchFactor(0, 20)
+        central_splitter.setStretchFactor(1, 30)
         
         # Connect signals
         self.ast_tree.currentItemChanged.connect(self.update_syntax_highlighting)
@@ -83,6 +103,32 @@ class AstViewer(QtGui.QMainWindow):
 
     # End of setup_methods
     # pylint: enable=W0201
+    
+    def new_file(self):
+        self.editor.clear()
+
+    def open_file(self, file_name=None):
+        if not file_name:
+            file_name = QtGui.QFileDialog.getOpenFileName(self, "Open File", '', "Python Files (*.py)")
+
+        logger.debug("Opening {}".format(file_name))
+        if file_name:
+            in_file = QtCore.QFile(file_name)
+            if in_file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
+                text = in_file.readAll()
+
+                try:
+                    # Python v3.
+                    text = str(text, encoding='ascii')
+                except TypeError:
+                    # Python v2.
+                    text = str(text)
+                print (text)
+                self.editor.setPlainText(text)
+            else: 
+                logger.warn("Unable to open: {}".format(file_name))
+                
+                
     
     def _fill_ast_tree(self):
         """ Fills the figure list widget with the titles/number of the figures
@@ -124,6 +170,8 @@ def main():
     app = QtGui.QApplication(sys.argv)
     
     parser = argparse.ArgumentParser(description='Python abstract syntax tree viewer')
+    parser.add_argument(dest='file_name', help='Python input file')
+    
     parser.add_argument('-l', '--log-level', dest='log_level', default = 'debug', 
         help = "Log level. Only log messages with a level higher or equal than this will be printed. Default: 'warn'",
         choices = ('debug', 'info', 'warn', 'error', 'critical'))
@@ -136,9 +184,10 @@ def main():
     logger.info('Started {}'.format(PROGRAM_NAME))
 
     
-    ast_viewer = AstViewer()
+    ast_viewer = AstViewer(file_name = args.file_name)
+    ast_viewer.resize(800, 600)
     ast_viewer.show()
-
+    
     exit_code = app.exec_()
     logging.info('Done {}'.format(PROGRAM_NAME))
     sys.exit(exit_code)
